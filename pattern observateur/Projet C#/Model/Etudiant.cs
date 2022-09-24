b49e2dc02e24f5eb4e9d72c6868fd0fc60241b33
gnehad <nehadghani@gmail.com>
2022-09-23 23:26:27 -0400

    Création du projet

---
+++ pattern observateur/Projet C#/Model/Etudiant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDP.Model
+{
+    public class Etudiant
+    {
+        public int IdEtudiant
+        {
+            set;get;
+        }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public double Moyenne { get; set; }
+
+        public Etudiant()
+        {
+
+        }
+
+        public Etudiant(int idEtudiant, string nom, string prenom, double moyenne)
+        {
+            IdEtudiant = idEtudiant;
+            Nom = nom; 
+            Prenom = prenom; 
+            Moyenne = moyenne; 
+        }
+
+        public override string ToString()
+        {
+            return "id : " + IdEtudiant + "\nNom : " + Nom + 
+                "\nPrénom : " + Prenom +"\nMoyenne : " + Moyenne + "\n";
+        }
+
+    }
+}
---
+++ pattern observateur/Projet C#/Model/Observable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDP.Model
+{
+    public interface Observable
+    {
+        void addObserver(Observer observer);
+        void removeObserver(Observer observer);
+        void notifyObservers();
+    }
+}
